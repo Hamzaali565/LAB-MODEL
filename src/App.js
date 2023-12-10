7e6f8f031f390341895a40928d8a7e82b4d468b4
Hamzaali565 <ha6153806@gmail.com>
2023-12-10 11:02:47 +0500

    side bar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Sidebar from "./components/SideBar/Sidebar";
+
+function App() {
+  return (
+    <div className="main">
+      <Sidebar />
+      <div className="container">hi!</div>
+    </div>
+  );
+}
+
+export default App;
