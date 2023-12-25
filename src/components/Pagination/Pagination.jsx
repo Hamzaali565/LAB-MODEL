d443c728b469c9c8a15d6f61c9fb66222a7e9416
Hamzaali565 <ha6153806@gmail.com>
2023-12-25 17:04:11 +0500

    Group Page Completed

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -4,6 +4,8 @@ import Stack from "@mui/material/Stack";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import Button from "../Buttons/Button";
+import Input from "../Inputs/Input";
 
 // let data = [
 //   { name: "hamza" },
@@ -54,11 +56,14 @@ import { useEffect } from "react";
 //   { name: "hamza" },
 // ];
 
-const PaginationComponent = ({ department }) => {
+const PaginationComponent = ({ department, sendToParent, resetFlag }) => {
   const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [data3, setData3] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(3);
   const url = useSelector((state) => state.url);
+
   const getData = async () => {
     try {
       const response = await axios.get(
@@ -66,6 +71,8 @@ const PaginationComponent = ({ department }) => {
       );
       console.log(response);
       setData(response.data.data);
+      setData2(response.data.data);
+      setData3(response.data.data);
       setPageNumber(0);
       department = null;
     } catch (error) {
@@ -75,26 +82,95 @@ const PaginationComponent = ({ department }) => {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    if (resetFlag) {
+      setData([]);
+      currentPageData = [];
+      console.log("currentPageData", currentPageData);
+    } else {
+      // Fetch data or update state as needed
+      getData();
+      console.log("currentPageData", currentPageData);
+    }
+  }, [resetFlag]);
   // Trigger the request when currentDepartment changes
 
   const totalPages = Math.ceil(data.length / itemsPerPage);
 
   const startIndex = pageNumber * itemsPerPage;
   const endIndex = Math.min((pageNumber + 1) * itemsPerPage, data.length);
-  const currentPageData = data.slice(startIndex, endIndex);
+  let currentPageData = data.slice(startIndex, endIndex);
 
   const handlePageChange = (event, newPageNumber) => {
     setPageNumber(newPageNumber - 1); // Adjusting for zero-based index
   };
+  const filterNames = (input) => {
+    let searchTerm = input.toLowerCase();
+    let d = data3.filter((item) =>
+      item.testName.toLowerCase().includes(searchTerm)
+    );
+    if (input.length <= 0) {
+      setData(data2);
+    } else {
+      setData(d);
+      setData3(data2);
+      setPageNumber(0); // Reset pageNumber to 0 when performing a search
+    }
+    return d;
+  };
+
+  let searchResult = "";
+  // console.log(searchResult);
+  const vector = (e) => {
+    // console.log(e.target.value);
+    searchResult = filterNames(e.target.value);
+  };
+  const handleClick = (item) => {
+    // Call the sendToParent callback with the clicked item
+    sendToParent(item);
+  };
 
   return (
     <div className="border-2 flex flex-col">
-      {/* Render your data here */}
-      {currentPageData.map((item, index) => (
-        <div key={index}>{item._id}</div>
-      ))}
+      {data.length > 0 && (
+        <h1 className=" flex justify-center underline my-2">
+          {department} Test List
+        </h1>
+      )}
+      <div className="flex justify-center my-2">
+        <Input
+          placeholder={"Search Test"}
+          onChange={(e) => {
+            vector(e);
+          }}
+        />
+      </div>
+      <div>
+        {/* First Div */}
+        <div className="border-2 mx-2 flex text-sm justify-around bg-black">
+          <p className="hidden md:flex">S.No</p>
+          <p className="hidden md:flex">Test Code</p>
+          <p className="">Test Name</p>
+          <p className="">Test Type</p>
+        </div>
 
-      <Stack spacing={2} className="flex self-center">
+        {/* Second Div */}
+        {data &&
+          currentPageData.map((item, index) => (
+            <div
+              key={index}
+              className="border-2 mx-2 flex text-sm justify-around cursor-pointer hover:scale-x-105 bg-gray-600"
+              onClick={() => handleClick(item)}
+            >
+              <p className="hidden md:flex">{index + 1}</p>
+              <p className="hidden md:flex">{item.testCode}</p>
+              <p className="">{item.testName}</p>
+              <p className="">{item.category}</p>
+            </div>
+          ))}
+      </div>
+      <Stack spacing={2} className="flex self-center my-3 ">
         <Pagination
           count={totalPages}
           page={pageNumber + 1}
@@ -102,7 +178,9 @@ const PaginationComponent = ({ department }) => {
           color="primary"
         />
       </Stack>
-      <button onClick={getData}>Call</button>
+      <div className="flex justify-center my-3">
+        <Button onClick={getData} title={"Load Tests"} />
+      </div>
     </div>
   );
 };
